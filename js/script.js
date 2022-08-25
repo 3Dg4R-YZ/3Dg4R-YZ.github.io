fac98c3fb1c6953431bf7693d8af47cb3b0a1ee0
3Dg4R-YZ <111653613+3Dg4R-YZ@users.noreply.github.com>
2022-08-24 22:44:23 -0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,18 @@
+alert("Hola amor");
+alert("Estaba pensando mientras hacia la pagina y decidi que creo que la hare un poco mas dinamica");
+alert("Ya sabes, para que no te duermas");
+while(1<2){
+	fat = prompt("Vamos a hacer un control rapido... che chinchera. Estas gordita?");
+	fat = fat.toUpperCase();
+	if(fat == "SI"){
+		alert("Bien, al menos lo reconoce jiijiji. Dale ya te dejo para que veas la pagina");
+		break;
+	}
+	else if(fat == "NO"){
+		alert("Mentirosaaaaaa, yo se que si lo estas");
+		break;
+	}
+	else{
+		alert("Escribe Si o No mi amor");
+	}
+}
